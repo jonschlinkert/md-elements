5184727278f5a4459b28168e21b823f32432fe53
jonschlinkert <jon.schlinkert@sellside.com>
2015-01-18 02:07:43 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+/*!
+ * md-elements <https://github.com/jonschlinkert/md-elements>
+ *
+ * Copyright (c) 2015 Jon Schlinkert, contributors.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = [
+  "article",
+  "aside",
+  "button",
+  "blockquote",
+  "body",
+  "canvas",
+  "caption",
+  "col",
+  "colgroup",
+  "dd",
+  "div",
+  "dl",
+  "dt",
+  "embed",
+  "fieldset",
+  "figcaption",
+  "figure",
+  "footer",
+  "form",
+  "h1",
+  "h2",
+  "h3",
+  "h4",
+  "h5",
+  "h6",
+  "header",
+  "hgroup",
+  "hr",
+  "iframe",
+  "li",
+  "map",
+  "object",
+  "ol",
+  "output",
+  "p",
+  "pre",
+  "progress",
+  "script",
+  "section",
+  "style",
+  "table",
+  "tbody",
+  "td",
+  "textarea",
+  "tfoot",
+  "th",
+  "tr",
+  "thead",
+  "ul",
+  "video"
+];
